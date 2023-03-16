a44d00fc8538bb8e34b21f3ca513fe3dc80d456c
GithubKolden <122537835+GithubKolden@users.noreply.github.com>
2023-03-16 19:52:08 +0100

    Sanity CLI
    created repo for cms system

---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'et4hr2p6',
+    dataset: 'production'
+  }
+})
---
+++ schemas/index.ts
@@ -0,0 +1,4 @@
+import Banner from './banner';
+import Product from './product';
+
+export const schemaTypes = [Banner, Product]
